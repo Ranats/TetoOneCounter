62fa5c731d1afbeef93efcfed8385ebf6d279672
Ranats <sopurani885@gmail.com>
2019-03-20 21:06:34 +0900

    test rmagick クロップ，拡大，二値化して保存 → rTesseractで認識． とりあえず22に関してはクリア

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require 'rtesseract'
+require 'rmagick'
 
-image = RTesseract.new("testData/6_trim_edit_bin34.png", psm: 8)
+img_src = "testData/22.jpg"
+
+original = Magick::Image.read(img_src).first
+
+# 絶対座標 → 1920x1080前提の座標 → %指定？ → 解像度取得，倍率指定
+image = original.crop(1120,85,165,105)
+image = image.scale(4)
+image = image.threshold(Magick::QuantumRange*0.34)
+image.write('crop.jpg')
+
+# メモリ解放
+image.destroy!
+
+image = RTesseract.new("crop.jpg", psm: 8)
 p image.to_box
