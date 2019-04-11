5accb33a353fc78e315f324e10974d5b7e8a3897
Satoshi Coda <sopurani885@gmail.com>
2019-04-11 19:01:44 +0900

    update app.rb ・テストデータのディレクトリ ・縦横サイズ指定

--- app.rb
+++ app.rb
@@ -7,9 +7,9 @@ img_src = "testData/22.jpg"
 def read (src)
 
   x = 1120.0 / 1920
-  y = 80.0 / 1080
+  y = 70.0 / 1080
   w = 165.0 / 1920
-  h = 120.0 / 1080
+  h = 135.0 / 1080
 
   original = Magick::Image.read(src).first
 
@@ -18,21 +18,21 @@ def read (src)
 
 # 絶対座標 → 1920x1080前提の座標 → %指定？ → 解像度取得，倍率指定
   image = original.crop(width*x, height*y, width*w, height*h)
-  image = image.scale(4)
-  image = image.threshold(Magick::QuantumRange*0.34)
+  image = image.scale(8)
+  image = image.threshold(Magick::QuantumRange*0.32)
   fname = 'croppedData/crop' + File.basename(src)
   image.write(fname)
 # メモリ解放
   image.destroy!
 
-  image = RTesseract.new(fname, psm: 8)
-  return image.to_s #image.to_box
+  image = RTesseract.new(fname, psm: 8, config_filie: :digits)
+  return image.to_box #image.to_box
 end
 
-dir = "testData"
+dir = "Data/testData"
 p Dir.foreach("testData")
 Dir.foreach(dir) do |file|
   next if [".",".."].include?(file)
-  p read(dir + "/" + file)
+  p file , read(dir + "/" + file)
 
 end
